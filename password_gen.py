4118ecbea9acb18588db54cb473eda7bc2082210
TeerthSonawani <82924733+TeerthSonawani@users.noreply.github.com>
2021-04-20 21:39:10 -0800

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -3,12 +3,11 @@ import pickle
 alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 up_alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 special_chars = ['!','@','#','$','%','^','&','*','~','+','-','_','?']
-def password(description):
-    def split(word):
-        return list(word)
+def password(description,digits):
     password = str()
     times = 0
-    ten = 10
+    digits = int(digits)
+    ten = digits
     k = list()
     global works
     works = False
@@ -26,45 +25,61 @@ def password(description):
             works = True 
             password += k[times]
             times += 1
+            password = str(password) 
         if choice == 1:
             up_low_choice = random.randint(0,1)
             if up_low_choice == 0:
                 works1 = True
                 alpha_choice = random.choice(alphabets)
                 password += alpha_choice
+                password = str(password) 
             if up_low_choice == 1:
                 works2 = True
                 alpha_choicee = random.choice(up_alphabets)
                 password += alpha_choicee
+                password = str(password) 
             times += 1        
         if choice == 2:
              works3 = True
              special_choice = random.choice(special_chars)
              password += special_choice
              times += 1
+             password = str(password) 
     if works == False:
         password = list(password)
         password.pop()
-        works = True 
         password += k[times]
         times += 1
+        password = str(password)
+        works = True
     if works1 == False:
         password = list(password)
         password.pop()
-        works1 = True
         alpha_choice = random.choice(alphabets)
         password += alpha_choice
+        password = str(password)
+        works1 = True
     if works2 == False:
         password = list(password)
         password.pop()
-        works2 = True
         alpha_choicee = random.choice(up_alphabets)
         password += alpha_choicee
+        password = str(password)
+        works2 = True
     if works3 == False:
         password = list(password)
         password.pop()
-        works3 = True
         special_choice = random.choice(special_chars)
         password += special_choice
+        password = str(password)
+        works3 = True
+    try:
+        password = eval(password)
+        if type(password) is list:
+            password = "".join(password)
+    except Exception:
+        pass
     password = str(password) 
-    print(password)
+    print(str(password))
+    
+    
